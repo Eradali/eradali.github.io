642adc3cbbd5e83a698caacbffc95a5aa69381f1
DESKTOP-VDV857I\erada <eradali34@yahoo.com>
2023-07-27 23:59:42 +0500

    Designed portfolio section

--- assets/js/main.js
+++ assets/js/main.js
@@ -202,15 +202,12 @@
   new Swiper('.portfolio-details-slider', {
     speed: 400,
     loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false
-    },
+    autoplay: false,
     pagination: {
       el: '.swiper-pagination',
       type: 'bullets',
       clickable: true
-    }
+    },
   });
 
   /**
